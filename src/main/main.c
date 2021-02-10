6257fac50463150fa0efad8ae3f71d913da40364
AlexsCode <alex.tuddenham.at@googlemail.com>
2021-02-10 15:55:08 +0000

    Updated WakePin Interrupt

--- src/main/main.c
+++ src/main/main.c
@@ -23,9 +23,9 @@
 #define HIGH 1  
 #define LOW 0
 #define LED_PIN GPIO_NUM_2 //Macro LED pin to onboard LED
-//#define WAKE_PIN RTC_GPIO10 //Configured to RTC setout of GPIO 04 Pin, unsure if matters for accessing ULP.
+
 #define ADC_PIN ADC2_CH1 //Configured ADC to GPIO 0
-#define WAKE_PIN ((gpio_num_t)GPIO_NUM_4)
+#define WAKE_PIN ((gpio_num_t)GPIO_NUM_4) // configuring Interupt pin, Accessible via RTC 
 
 //Creating Flag accessible from RTC memory.
 RTC_DATA_ATTR bool SleepFlag=false;
@@ -48,7 +48,7 @@ void app_main(void)
 
     else{
         //Staying Awake Code.
-        //TaskCreate(&rtos_Test,"testing methods",8048,NULL,5,NULL);
+        
         printf("debugging loop");
         fflush(stdout);
     }
